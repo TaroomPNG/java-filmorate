9234cf9cc7626eb03f4f5f4b8c0343c38c69c40e
Taroom <T4room@yandex.ru>
2026-09-08 01:37:22 +0300

    Add feed (#6)
    * Add user activity feed (GET /users/{id}/feed)
    
    * Add Feed tests
    
    * Add FeedService for Review
    
    * The logic for Postman automated tests has been fixed
    
    * Fixing pull request comments (Tests, query optimization)

--- src/test/java/ru/yandex/practicum/filmorate/DaoTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/DaoTest.java
@@ -8,21 +8,23 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
-import ru.yandex.practicum.filmorate.controller.service.storage.DirectorDbStorage;
+import ru.yandex.practicum.filmorate.controller.service.FeedService;
+import ru.yandex.practicum.filmorate.controller.service.FilmService;
+import ru.yandex.practicum.filmorate.controller.service.storage.FeedDbStorage;
 import ru.yandex.practicum.filmorate.controller.service.storage.FilmDbStorage;
 import ru.yandex.practicum.filmorate.controller.service.storage.ReviewDbStorage;
 import ru.yandex.practicum.filmorate.controller.service.storage.UserDbStorage;
-import ru.yandex.practicum.filmorate.controller.service.storage.mapper.DirectorRowMapper;
+import ru.yandex.practicum.filmorate.controller.service.storage.mapper.FeedRowMapper;
 import ru.yandex.practicum.filmorate.controller.service.storage.mapper.FilmRowMapper;
 import ru.yandex.practicum.filmorate.controller.service.storage.mapper.ReviewRowMapper;
 import ru.yandex.practicum.filmorate.controller.service.storage.mapper.UserRowMapper;
-import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Rating;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.model.dto.directorDto.DirectorPostRequest;
+import ru.yandex.practicum.filmorate.model.dto.feedDto.FeedPostRequest;
 import ru.yandex.practicum.filmorate.model.dto.filmDto.FilmPostRequest;
 import ru.yandex.practicum.filmorate.model.dto.reviewDto.ReviewPostRequest;
 import ru.yandex.practicum.filmorate.model.dto.userDto.UserPostRequest;
@@ -33,11 +35,13 @@ import ru.yandex.practicum.filmorate.model.dto.userDto.UserPostRequest;
   UserDbStorage.class,
   FilmDbStorage.class,
   ReviewDbStorage.class,
-  DirectorDbStorage.class,
+  FeedDbStorage.class,
   UserRowMapper.class,
   FilmRowMapper.class,
-  ReviewRowMapper.class,
-  DirectorRowMapper.class
+  FeedRowMapper.class,
+  FilmService.class,
+  FeedService.class,
+  ReviewRowMapper.class
 })
 @TestPropertySource(
     properties = {
@@ -49,15 +53,19 @@ public abstract class DaoTest {
 
   @Autowired protected UserDbStorage userStorage;
   @Autowired protected FilmDbStorage filmStorage;
+  @Autowired protected FeedDbStorage feedStorage;
   @Autowired protected ReviewDbStorage reviewStorage;
-  @Autowired protected DirectorDbStorage directorStorage;
 
   @BeforeEach
   void resetData() {
+    feedStorage.clear();
     reviewStorage.clear();
     filmStorage.clear();
-    directorStorage.clear();
     userStorage.clear();
+  }
+
+  protected Long addFeed(Long userId, EventType type, Operation operation, Long entityId) {
+    return feedStorage.addFeed(new FeedPostRequest(userId, type, operation, entityId));
   }
 
   protected User addUser(String email, String login) {
@@ -82,12 +90,6 @@ public abstract class DaoTest {
             .genres(genres)
             .mpa(mpa)
             .build());
-  }
-
-  protected Director addDirector(String name) {
-    DirectorPostRequest request = new DirectorPostRequest();
-    request.setName(name);
-    return directorStorage.addDirector(request);
   }
 
   protected Review addReview(String content, Boolean isPositive, Long userId, Long filmId) {
