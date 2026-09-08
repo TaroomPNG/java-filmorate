39ce391894a48f7085808e04475e49c533876cc9
Dion <dinisuali17@yandex.ru>
2026-09-09 02:16:49 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/service/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/service/storage/FilmDbStorage.java
@@ -10,7 +10,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.controller.exceptions.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.controller.exceptions.FilmNotFound;
 import ru.yandex.practicum.filmorate.controller.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.controller.service.storage.mapper.FilmRowMapper;
