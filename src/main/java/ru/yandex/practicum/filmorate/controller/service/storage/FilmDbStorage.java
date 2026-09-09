3e7a60a366bb60a044e38f731a5fd0880987fe1e
DanDeLion-Ya <dinisuali17@yandex.ru>
2026-09-09 22:49:17 +0300

    Update FilmDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/service/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/service/storage/FilmDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller.service.storage;
 
 import java.util.*;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,6 +11,7 @@ import ru.yandex.practicum.filmorate.controller.exceptions.ConditionsNotMetExcep
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.controller.exceptions.FilmNotFound;
 import ru.yandex.practicum.filmorate.controller.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.controller.service.storage.mapper.FilmRowMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Rating;
@@ -87,17 +89,36 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
       "MERGE INTO film_likes (user_id, film_id) KEY (user_id, film_id) VALUES (?, ?)";
   private static final String DELETE_LIKE =
       "DELETE FROM film_likes WHERE film_id = ? AND user_id = ?";
-
+  
   private static final String FIND_LIKES_BY_FILM =
       "SELECT u.* FROM \"user\" AS u "
           + "JOIN film_likes AS fl ON u.user_id = fl.user_id WHERE fl.film_id = ?";
-
+  
+  private static final String FIND_COMMON_FILMS =
+      FILM_SELECT
+          + " INNER JOIN film_likes AS fl1 ON f.film_id = fl1.film_id "
+          + "INNER JOIN film_likes AS fl2 ON f.film_id = fl2.film_id "
+          + "LEFT JOIN film_likes AS fl ON f.film_id = fl.film_id "
+          + "WHERE fl1.user_id = ? AND fl2.user_id = ? "
+          + "GROUP BY f.film_id, f.name, f.description, f.release_date, f.duration, r.rating_id, r.rating "
+          + "ORDER BY COUNT(fl.user_id) DESC, f.film_id;";
+  
   private static final String SEARCH_FILMS =
       FILM_SELECT
           + " LEFT JOIN film_to_directors AS ftd ON f.film_id = ftd.film_id "
           + "LEFT JOIN director AS d ON ftd.director_id = d.director_id "
           + "LEFT JOIN film_likes AS fl ON f.film_id = fl.film_id ";
-
+  
+  private static final String GET_RECOMMENDATION_FILMS =
+      FILM_SELECT + " JOIN film_likes AS fl ON f.film_id = fl.film_id " +
+            "WHERE fl.user_id IN (SELECT fl2.user_id FROM film_likes AS fl1 " +
+            "JOIN film_likes AS fl2 ON fl1.film_id = fl2.film_id AND fl1.user_id != fl2.user_id " +
+            "WHERE fl1.user_id = ? " +
+            "GROUP BY fl2.user_id " +
+            "ORDER BY COUNT(fl2.film_id) DESC, fl2.user_id " +
+            "LIMIT 1) " +
+            "AND fl.film_id NOT IN (SELECT film_id FROM film_likes WHERE user_id = ?)";
+  
   private final RowMapper<Genre> genreMapper =
       (rs, rowNum) -> new Genre(rs.getInt("genre_id"), rs.getString("genre"));
   private final RowMapper<Rating> ratingMapper =
@@ -339,6 +360,17 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
     return films;
   }
 
+  @Override
+  public List<Film> getRecommendation(Long id) {
+      try {
+          List<Film> recFilms = jdbc.query(GET_RECOMMENDATION_FILMS, mapper, id, id);
+          fillGenresAndDirectors(recFilms);
+          return recFilms;
+      } catch (EmptyResultDataAccessException e) {
+          throw new NotFoundException("Пользователь с id " + id + " не найден");
+      }
+  }
+
   private void fillGenresAndDirectors(List<Film> films) {
     if (films.isEmpty()) {
       return;
@@ -410,6 +442,17 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
     List<Object[]> params =
         uniqueIds.stream().map(genreId -> new Object[] {filmId, genreId}).toList();
     jdbc.batchUpdate(ADD_FILM_TO_GENRES, params);
+  }
+
+  @Override
+  public List<Film> getCommonFilms(long userId, long friendId) {
+    List<Film> films = jdbc.query(FIND_COMMON_FILMS, new FilmRowMapper(), userId, friendId);
+      if (films.isEmpty()) {
+          return films;
+      }
+
+      fillGenresAndDirectors(films);
+      return films;
   }
 
   @Override
